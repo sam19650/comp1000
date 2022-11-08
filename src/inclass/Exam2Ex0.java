0936d2febd9ba38ef8a72abcedbc52af68548e04
sam19650 <sam19650@yahoo.com>
2022-11-08 13:37:16 -0500

    continuous improvement

--- src/inclass/Exam2Ex0.java
+++ src/inclass/Exam2Ex0.java
@@ -16,7 +16,29 @@ public class Exam2Ex0 {
 		double x2 = scanner.nextDouble();
 		
 		// Process
-
+		double min, max;
+		
+		if (x0 > x1) {
+			min = x1;
+			max = x0;
+		} else {
+			min = x0;
+			max = x1;
+		}
+		
+		if (x2 < min) {
+			x0 = x2;
+			x1 = min;
+			x2 = max;
+		} else if (x2 > max) {
+			x0 = min;
+			x1 = max;
+		} else {
+			x0 = min;
+			x1 = x2;
+			x2 = max;
+		}
+		
 		// Output
 		System.out.println("x0 = " + x0);
 		System.out.println("x1 = " + x1);
