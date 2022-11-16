20e2252f676adc4dc4d4ca9c7da0588f55b7390d
sam19650 <sam19650@yahoo.com>
2022-11-16 15:15:26 -0500

    continuous improvement

---
+++ src/inclass/ReadIntegersFromFile.java
@@ -0,0 +1,40 @@
+package inclass;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadIntegersFromFile {
+
+	public static final String FILE_NAME = "data.txt";
+	
+	public static void main(String[] args) {
+
+		File file = new File(FILE_NAME);
+		
+		try {
+			Scanner scanner = new Scanner(file);
+
+			int sum = 0;
+			int count = 0;
+			
+			while (scanner.hasNextInt()) {
+				int value = scanner.nextInt();
+				sum += value;
+				count++;
+			}			
+			scanner.close();
+
+			int average = sum / count;
+			
+			System.out.printf("Sum = %d%n", sum);
+			System.out.printf("Average = %d%n", average);
+			
+		} catch (FileNotFoundException exception) {
+			System.out.printf("%s not found!%n", FILE_NAME);
+			System.exit(0);
+		}
+
+	}
+
+}
